77d6c5fe5bd8a63a85ee17e82f4ecf45270341b3
heygwangjin <cer3@ajou.ac.kr>
2021-10-08 17:23:06 +0900

    refactor: Add drawQuestionScene(), hideQuestionScene(), hideMainScene()     - 각 함수의 이름에 맞게 구현 했습니다! 보시고 이상한 것 있으면       얘기해 주세요!     - 그리고, 유미씨꺼 안됐었던거 p5js-tmp~~ 라고 시작하는 파일이랑 같이       업로드 하셔서 그런거 같네요. 이게 아마 브라우저에서 코드 실행할 때       생기는 tmp 파일인 것 같은데, 깃에 add 하실 때 확인 부탁드려요!(gitignore 파일에 추적 못하게 적어 놨는데 제대로 되는지는         모르겠네요!)

--- cmp_processing.js
+++ cmp_processing.js
@@ -1,17 +1,17 @@
 /* Constants */
 const Y_AXIS = 1; // axis of gardient
 const X_AXIS = 2;
-const WIDTH_CANVAS = 1500;
-const HEIGHT_CANVAS = 900;
+const WIDTH_CANVAS = 1450;
+const HEIGHT_CANVAS = 800;
 
 /* Buttons */
 let btnPlay, btnAnswer, btnWrongOne, btnWrongTwo, btnNext;
 /* Values for creating button */
-let contentBtn, xPosBtn, yPosBtn, widthBtn, heightBtn;
+let textBtn, xPosBtn, yPosBtn, widthBtn, heightBtn;
 /* Font of fancy text */
 let font;
 /* Variables to store fancy text */
-let title, questionOne, questionTwo, questionTwo_2, questionThree;
+let title, questionOne, questionOne_2, questionTwo, questionTwo_2, questionThree, questionThree_2;
 
 const manager = new Manager(WIDTH_CANVAS, HEIGHT_CANVAS);
 
@@ -22,254 +22,59 @@ function preload() {
 }
 
 function setup() {
-  // Display initial screen
+  manager.setInit();
   manager.displayInitScreen();
-
-  // Instanciate title and questions
-  title = manager.generateFancyText("QUESTION BOARD", 400, 200, 80, font);
-  questionOne = manager.generateFancyText(
-    "Choose the animal which lives in 'Antarctica'",
-    100,
-    200,
-    60,
-    font
-  );
-  questionTwo = manager.generateFancyText(
-    "Choose the 'north gate'",
-    100,
-    200,
-    80,
-    font
-  ); // First line
-  questionTwo_2 = manager.generateFancyText(
-    "from Suwon Hwaseong fortress",
-    100,
-    300,
-    80,
-    font
-  ); // Second line
-  questionThree = manager.generateFancyText(
-    "Choose the character who sang ",
-    100,
-    200,
-    80,
-    font
-  );// First Line
-
-  questionThree_2 = manager.generateFancyText(
-    "‘show yourself’ in movie of the Frozen?",
-    100,
-    300,
-    70,
-    font
-  ); // Second Line
-  title.need = 1; // We need to display title when we start our app.
 }
 
 function draw() {
   /* Draw background */
-  manager.setGradient(
-    0,
-    0,
-    manager.width,
-    manager.height,
-    color(65),
-    color(50),
-    Y_AXIS
-  );
+  // TODO : 각자 기능 화면 실행할 때는 수행 안되게 하기
+  manager.setGradient(0, 0, manager.width, manager.height, color(65), color(50), Y_AXIS);
 
   /* Scene flow. */
   if (title.need) {
     title.drawFancyText();
-  } else if (questionOne.need) {
+  } else if (questionOne.need && questionOne_2.need) {
     questionOne.drawFancyText();
-  } else if (questionTwo.need) {
+    questionOne_2.drawFancyText();
+  } else if (questionTwo.need && questionTwo_2.need) {
     questionTwo.drawFancyText();
     questionTwo_2.drawFancyText();
-  } else if (questionThree.need) {
+  } else if (questionThree.need && questionThree_2.need) {
     questionThree.drawFancyText();
     questionThree_2.drawFancyText();
   }
 
-  /* Scene01 -> Scene02 */
+  /* Initial Scene -> Question01 */
   btnPlay.mousePressed(() => {
-    // Hide title and playbutton
-    question1_op();
-    // Move to the function execution screen when answer button is clicked
+    manager.drawQuestionScene(questionOne, questionOne_2, "Penguin", "Arctic bear", "Arctic fox");
+
+    /* Question01 -> Question01-Main */
     btnAnswer.mousePressed(() => {
-      response1();
-      /*Scene02 -> Scene03*/
+      manager.hideQuestionScene(questionOne, questionOne_2, btnAnswer, btnWrongOne, btnWrongTwo);
+
+      /* Question01-Main -> Question02 */
       btnNext.mousePressed(() => {
-       question2_op();
-       btnAnswer.mousePressed(() => {
-         response2();
-         /*Scene03 -> end*/
-         btnNext.mousePressed(() => {
-           question3_op();
-           btnAnswer.mousePressed(() => {
-             alert('success!');
-           });
-         });
-       });
+        manager.drawQuestionScene(questionTwo, questionTwo_2, "Paldalmun Gate", "Janganmun Gate", "Hwaseomun Gate");
+        manager.hideMainScene(btnNext);
+
+        /* Question02 -> Question02-Main */
+        btnAnswer.mousePressed(() => {
+          manager.hideQuestionScene(questionTwo, questionTwo_2, btnAnswer, btnWrongOne, btnWrongTwo);
+
+          /* Question02-Main -> Question03*/
+          btnNext.mousePressed(() => {
+            manager.drawQuestionScene(questionThree, questionThree_2, "Anna", "Elsa", "Olaf");
+            manager.hideMainScene(btnNext);
+
+            /* Question03 -> Question03-Main */
+            btnAnswer.mousePressed(() => {
+              manager.hideQuestionScene(questionThree, questionThree_2, btnAnswer, btnWrongOne, btnWrongTwo);
+              manager.hideMainScene(btnNext);
+            });
+          });
+        });
       });
     });
   });
-}
-function question1_op(){
-    manager.hideFancyText(title);
-    manager.hideButton(btnPlay);
-
-    // Display question one and multiple choices
-    manager.displayFancyText(questionOne);
-
-    // Display multiple choices
-    manager.setValuesBtn("Arctic bear", 600, 400, 320, 70);
-    btnWrongOne = manager.displayButton(
-      contentBtn,
-      xPosBtn,
-      yPosBtn,
-      widthBtn,
-      heightBtn
-    );
-
-    manager.setValuesBtn("Penguin", 600, 500, 320, 70);
-    btnAnswer = manager.displayButton(
-      contentBtn,
-      xPosBtn,
-      yPosBtn,
-      widthBtn,
-      heightBtn
-    );
-
-    manager.setValuesBtn("Arctic fox", 600, 600, 320, 70);
-    btnWrongTwo = manager.displayButton(
-      contentBtn,
-      xPosBtn,
-      yPosBtn,
-      widthBtn,
-      heightBtn
-    );
-}
-function question2_op(){
-   // Hide nextBtn
-    manager.hideButton(btnNext);
-
-   // Display question two and multiple choices
-   manager.displayFancyText(questionTwo);
-   manager.displayFancyText(questionTwo_2);
-
-  // Display multiple choices
-  manager.setValuesBtn("Paldalmun Gate", 600, 400, 320, 70);
-        btnWrongOne = manager.displayButton(
-          contentBtn,
-          xPosBtn,
-          yPosBtn,
-          widthBtn,
-          heightBtn
-        );
-
-        manager.setValuesBtn("Janganmun Gate", 600, 500, 320, 70);
-        btnAnswer = manager.displayButton(
-          contentBtn,
-          xPosBtn,
-          yPosBtn,
-          widthBtn,
-          heightBtn
-        );
-
-        manager.setValuesBtn("Hwaseomun Gate", 600, 600, 320, 70);
-        btnWrongTwo = manager.displayButton(
-          contentBtn,
-          xPosBtn,
-          yPosBtn,
-          widthBtn,
-          heightBtn
-        );
-}
-function question3_op(){
-   // Hide nextBtn
-    manager.hideButton(btnNext);
-
-   // Display question two and multiple choices
-   manager.displayFancyText(questionThree);
-   manager.displayFancyText(questionTwo_2);
-
-  // Display multiple choices
-  manager.setValuesBtn("Anna", 600, 400, 320, 70);
-        btnWrongOne = manager.displayButton(
-          contentBtn,
-          xPosBtn,
-          yPosBtn,
-          widthBtn,
-          heightBtn
-        );
-
-        manager.setValuesBtn("Elsa", 600, 500, 320, 70);
-        btnAnswer = manager.displayButton(
-          contentBtn,
-          xPosBtn,
-          yPosBtn,
-          widthBtn,
-          heightBtn
-        );
-
-        manager.setValuesBtn("White snow", 600, 600, 320, 70);
-        btnWrongTwo = manager.displayButton(
-          contentBtn,
-          xPosBtn,
-          yPosBtn,
-          widthBtn,
-          heightBtn
-        );
-}
-function response1(){
-   // Hide questionOne, answerBtn and wrongBtn
-      manager.hideFancyText(questionOne);
-      manager.hideButton(btnWrongOne);
-      manager.hideButton(btnWrongTwo);
-      manager.hideButton(btnAnswer);
-
-      // On the function execution screen, click the next button
-      manager.setValuesBtn("Next Question", width - 250, height - 200, 200, 70);
-      btnNext = manager.displayButton(
-        contentBtn,
-        xPosBtn,
-        yPosBtn,
-        widthBtn,
-        heightBtn
-      );
-}
-function response2(){
-   // Hide questionOne, answerBtn and wrongBtn
-      manager.hideFancyText(questionTwo);
-      manager.hideButton(btnWrongOne);
-      manager.hideButton(btnWrongTwo);
-      manager.hideButton(btnAnswer);
-
-      // On the function execution screen, click the next button
-      manager.setValuesBtn("Next Question", width - 250, height - 200, 200, 70);
-      btnNext = manager.displayButton(
-        contentBtn,
-        xPosBtn,
-        yPosBtn,
-        widthBtn,
-        heightBtn
-      );
-}
-function response3(){
-   // Hide questionOne, answerBtn and wrongBtn
-      manager.hideFancyText(questionThree);
-      manager.hideButton(btnWrongOne);
-      manager.hideButton(btnWrongTwo);
-      manager.hideButton(btnAnswer);
-
-      // On the function execution screen, click the next button
-      manager.setValuesBtn("Next Question", width - 250, height - 200, 200, 70);
-      btnNext = manager.displayButton(
-        contentBtn,
-        xPosBtn,
-        yPosBtn,
-        widthBtn,
-        heightBtn
-      );
 }
