f35ff48e317f2367bb9bcbbf591958a913159c86
Gwangjin Kim <cer3@ajou.ac.kr>
2021-10-20 21:45:55 +0900

    feat: Add the game restarting and levels of the game (#17)
    - replay(), changeLevel(), setDifficulty()

--- ManagerGame.js
+++ ManagerGame.js
@@ -2,12 +2,24 @@
  * Created by Gwang Jin Kim on 10/14/21.
  * This class is responsible for managing about penguin scene.
  */
+
+/* CONSTANTS */
+const RIGHT = 0;
+const LEFT = 180;
+const THOUSAND = 1000;
+
 let penguin;
 let snowballs = [];
+let time, speedSnowball, scaleSnowball;
 
 class ManagerGame {
   constructor() {
     this.isEnd = 0;
+    this.startingTime = 0;
+    this.overTime = 0;
+    this.elapsedTime = 0;
+    this.win = 0;
+    this.lose = 0;
   }
   /**
    * Draw initial scene of penguin game.
@@ -34,7 +46,7 @@ class ManagerGame {
    * @param {Number} f friction of penguin.
    */
   createPenguin(w, h, s, f) {
-    penguin = createSprite(w, h); penguin.scale = s;
+    penguin = createSprite(w, h); penguin.scale = s; //penguin.debug = true;
     penguin.setCollider("rectangle", 0, 0, 170, 310);
     penguin.friction = f;
     penguin.addAnimation("penguin", "data/penguin.png");
@@ -62,15 +74,25 @@ class ManagerGame {
     };
   }
 
+  controlPenguin() {
+    if (keyIsPressed === true) {
+      if (key === "z") {
+        penguin.rotation -= 10;
+        penguin.velocity.x = random(-10, 10);
+        penguin.velocity.y = random(-10, 10);
+      }
+    }
+  }
+
   /**
    * Make snowball fall every t seconds after create it.
    * @param {Number} t seconds that make snow fall.
    * @param {Number} sp scalar speed to add at which snow fall.
-   * @param {Number} sc at which snow create.
+   * @param {Number} sc scale at which snow create.
    */
   createSnowball(t, sp, sc) {
-    if (millis() % t === 0) {
-      const snowball = createSprite(random(0, WIDTH_CANVAS), 20); snowball.scale = sc;
+    if (millis() % t === 0 && !this.isEnd) {
+      const snowball = createSprite(random(0, WIDTH_CANVAS), 20); snowball.scale = sc; snowball.debug = true;
       snowball.setCollider("circle", 0, 0, 300);
       snowball.addAnimation("snowball", "data/snowball.png");
       snowball.addSpeed(sp, 90);
@@ -80,41 +102,92 @@ class ManagerGame {
   }
 
   /**
-   * Remove snowball if it passes penguin.
+   * Remove snowball whithin a specified range.
+   * @param {Number} range range of snowball that you want to remove.
    */
-  removeSnowball() {
-    snowballs.forEach(snowball => {
-      if (snowball.newPosition.y >= penguin.position.y) {
-          snowball.remove();
-          snowballs.shift();
-      }
-    })
+  removeSnowball(range) {
+    snowballs.forEach(snowball => {if (snowball.position.y >= range) snowball.remove()});
   }
 
   /**
    * Check game over.
    */
   checkGameOver() {
-    snowballs.forEach(snowball => {if (snowball.overlap(penguin)) penguin.remove();});
+    snowballs.forEach(snowball => {
+      if (snowball.overlap(penguin)) {
+        penguin.remove();
+        this.lose = 1;
+      }
+    });
 
-    if (penguin.removed) {
-        this.isEnd = 1;
-        
-        if (this.isEnd) {
-          textSize(26);
-          text("GAME OVER!", WIDTH_CANVAS/2, 40);
-          text("Press 'R' if you want to see the penguin again!", WIDTH_CANVAS/2, 100);
-        }
+    /* LOSE !! */
+    if (this.lose) {
+      this.isEnd = 1;
+
+      textSize(26);
+      text("GAME OVER 😢", WIDTH_CANVAS/2, 40);
+      text("Press 'r' if you want to see the penguin again! 🐧", WIDTH_CANVAS/2, 100);
+    } else if(this.win) {
+      this.isEnd = 1;
+
+      textSize(26);
+      text("CONGRATULATIONS 🎉", WIDTH_CANVAS/2, 40);
+      text("You win the game 🏆", WIDTH_CANVAS/2, 100);
+      text("Press 'r' if you want to play with penguin again! 🐧", WIDTH_CANVAS/2, 150);
     }
   }
 
-  controlPenguin() {
-    if (keyIsPressed === true) {
-      if (key === "z") {
-        penguin.rotation -= 10;
-        penguin.velocity.x = random(-10, 10);
-        penguin.velocity.y = random(-10, 10);
-      }
+  /**
+   * Set variables in the difficulty of the game.
+   * @param {Number} t seconds that make snow fall.
+   * @param {Number} sp scalar speed to add at which snow fall.
+   * @param {Number} sc scale at which snow create.
+   */
+  setDifficulty(t, sp, sc) {
+    time = t;
+    speedSnowball = sp;
+    scaleSnowball = sc;
+  }
+
+  /**
+   * Change the level of the game depending on elapsed time.
+   */
+  changeLevel() {
+    this.elapsedTime = (millis() / THOUSAND) - this.overTime;
+
+    if (this.elapsedTime <= 10) {
+      this.setDifficulty(3, 6, .1);
+    } else if (this.elapsedTime <= 20) {
+      this.setDifficulty(2, 8, .15);
+    } else if (this.elapsedTime <= 30) {
+      this.setDifficulty(1.5, 10, .2);
+    } else if (this.elapsedTime <= 40) {
+      this.setDifficulty(1.5, 12, .24);
+    } else if (this.elapsedTime <= 50) {
+      this.setDifficulty(1.3, 13, .27);
+    } else if (this.elapsedTime <= 65) {
+      this.setDifficulty(1.2, 14, .3);
+    } else if (this.elapsedTime > 65) {
+      this.win = 1;
+    }
+  }
+
+  /**
+   * Replay the game.
+   */
+  replay() {
+    if (keyIsPressed === true && key === "r" && this.isEnd) {
+      if (!penguin.removed) penguin.remove();
+
+      this.isEnd = 0;
+      this.lose = 0;
+      this.win = 0;
+
+      this.overTime = millis() / THOUSAND; // update game over time.
+
+      this.createPenguin(WIDTH_CANVAS/2, HEIGHT_CANVAS - 170, .5, .11);
+      this.removeSnowball(0);
+      snowballs.length = 0;
     }
   }
 }
