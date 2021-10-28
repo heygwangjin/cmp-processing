5e76d9b35e7315c8f07ae68f8c1d11ed8caffc1f
yumi-jun <junyumi1006@naver.com>
2021-10-29 00:25:42 +0900

    re

--- Manager.js
+++ Manager.js
@@ -63,6 +63,7 @@ class Manager {
     needGradient = 1;
     needPenguinScene = 0;
     needSnowScene=0;
+    needFinalScene=0;
   }
 
   displayInitScreen() {
@@ -203,7 +204,17 @@ class Manager {
   turnOnSnowScene(){
     needSnowScene = 1;
   }
+  turnOffSnowScene(){
+    needSnowScene = 0;
+  }
 
+   turnOnFinalScene(){
+    needFinalScene = 1;
+  }
+
+  turnOffFinalScene(){
+    needFinalScene = 0;
+  }
   /* Hide btn and main scene */
   hideMainScene(btn){
     this.hideBtn(btn);
